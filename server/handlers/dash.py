41e02b9d764e3bc6b95216996441c2fadb153473
Jeff Carollo <jeffc@utest.com>
2012-10-16 15:08:10 -0600

    Change Dash to reflect new Executor names.

--- server/handlers/dash.py
+++ server/handlers/dash.py
@@ -17,6 +17,7 @@ __author__ = 'jeff.carollo@gmail.com (Jeff Carollo)'
 import datetime
 import json
 import logging
+import urllib
 import webapp2
 
 from google.appengine.ext.webapp import template
@@ -25,19 +26,30 @@ from models import tasks
 from models import packages
 from util import model_to_dict
 
-
+# Old Devices IDs.
+#  'SH0CJLV00997',
+#  '3233A90D16A800EC',
+#  '328C000600000001',
+#  '902a6d03',
+#  'LGOTMSae4105e',
+#  '0146A14C1001800C',
+#  'HT16RS015741',
+#  'TA08200CI0',
+#  '4342354131485A483144',
+#  '388920443A07097',
+#  '304D191A2004639E',
 DEVICES = [
-  'SH0CJLV00997',
-  '3233A90D16A800EC',
-  '328C000600000001',
-  '902a6d03',
-  'LGOTMSae4105e',
-  '0146A14C1001800C',
-  'HT16RS015741',
-  'TA08200CI0',
-  '4342354131485A483144',
-  '388920443A07097',
-  '304D191A2004639E',
+  'Amazon Kindle Fire',
+  'Galaxy Nexus',
+  'Google Nexus S',
+  'HTC Thunderbolt',
+  'LG Nitro HD',
+  'Motorola Droid X2',
+  'Samsung Galaxy S II',
+  'Samsung Galaxy Tab',
+  'Samsung Galaxy Tab 8.9',
+  'Sony Ericson Xperia PLAY 4G',
+  'T-Mobile MyTouch 3G',
   'macos',
 ]
 
@@ -54,6 +66,7 @@ class DashHandler(webapp2.RequestHandler):
 class ExecutorDashHandler(webapp2.RequestHandler):
   """A dashboard for MrTaskman."""
   def get(self, executor):
+    executor = urllib.unquote(executor)
     backlog = tasks.GetByExecutor(executor, limit=5000, keys_only=True)
     oldest = tasks.GetOldestTaskForCapability(executor)
     recently_finished = tasks.GetRecentlyFinishedTasks(executor, 20)
@@ -88,5 +101,5 @@ class ExecutorDashHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/dash', DashHandler),
-    ('/dash/([0-9a-zA-Z]+)', ExecutorDashHandler),
+    ('/dash/(.+)', ExecutorDashHandler),
     ], debug=True)
